4b719178fab5d7af2d79bc095eb7d20a0d6ad0e4
Daniel MBAPPE <mbappedaniel00@yahoo.fr>
2025-08-05 15:41:16 +0200

    - update config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
